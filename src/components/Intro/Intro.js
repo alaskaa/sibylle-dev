3fda7b8fc23a9cf6840260dce61b0950cecf2058
Sibylle Sehl <sibylle.sehl@googlemail.com>
2021-07-04 14:39:51 +0200

    Update intro to reflect job change

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -5,7 +5,7 @@ import IntroWrapper from "./components/IntroWrapper"
 const Intro = () => (
   <IntroWrapper>
     <WhiteHeadingWrapper>Hi, I'm Sibylle</WhiteHeadingWrapper>
-    <p>Frontend Developer based in Berlin.</p>
+    <p>Software Engineer based in Berlin.</p>
     <br />
     <p>
       Curious and always eager to learn about new and better ways to solve a
@@ -16,10 +16,10 @@ const Intro = () => (
       I'm excited about development solutions that feel intuitive and provide
       better mental models for our abstractions.
     </p>
-    <p>Currently really into React and GraphQL.</p>
+    <p>Currently really into React and GraphQL. I'm also learning PHP.</p>
     <p>
       When I'm not debugging an error message or crafting the next component,
-      I'm sat with a good book & a cup of tea, learning Japanese or finding some
+      I'm either sat with a good book & a cup of tea or finding some
       beautiful spots in the wilderness.
     </p>
   </IntroWrapper>
